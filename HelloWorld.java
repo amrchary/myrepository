910873090ea7927480006fe1b55e585ea95dc77e
amrchary <amrchary@gmail.com>
2018-03-03 11:25:47 -0800

    Added some comments.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 import java.util.*;
 
+/**
+ * First program, prints out "Hello World!".
+ */
 public class HelloWorld {
 
+  /* main method */
   public static final void main(String args[]) {
     System.out.println("Hello World!");
     System.exit(0);
